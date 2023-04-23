fba8be227266963162b883d15bd29302dc73b6e5
Ryohei <harrry.kuma1225@gmail.com>
2023-04-24 00:53:15 +0900

    commit

--- src/libs/blog-link.ts
+++ src/libs/blog-link.ts
@@ -1,5 +1,3 @@
 export const getPageLink = (tag:string,page:number) =>{
-  return tag ? `/Post/tag/${tag}/page/${page}`:`/Post/${page}/1`
+  return tag ? `/Post/tag/${tag}/page/${page}`:`/Post/${page}`
 }
-// ページネーションの所は一つにして、それぞれのURLに遷移する様にここで書いている
-// /Post/tag/blog/page/1
